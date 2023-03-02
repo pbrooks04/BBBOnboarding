ddf5fd47b9bacaab5e3730d394a037a2614a959f
Peter Brooks <thepj04@gmail.com>
2023-03-02 15:47:15 -0500

    Animation with text and toggle height for event table

--- src/modules/script.js
+++ src/modules/script.js
@@ -1,38 +1,68 @@
 let eventHistory = [];
-const logHistory = (event, type) => eventHistory.push({ event, type })
+const logHistory = (event, type) => {
+  console.log('Adding Event: ', event);
+  eventHistory.push({ ...event, type });
+  $('#jqxgrid').jqxGrid('updatebounddata');
+}
 
 const handleAlertClick = e => {
   logHistory(e, 'Alert');
   alert('Consider yourself alerted');
 };
 const handleAnimationClick = e => {
-  console.log('Click event ', e)
   logHistory(e, 'Animation');
-  // TODO
-  // Do some animation
 
-  // Init animate div
-  // Set location, size, etc
+  const animationTime = 3000;
+  $('#animation').attr('hidden', false);
+  $('#p_animation').attr('hidden', false);
 
-  // Grow div (square) from "nowhere"
+  $('#animation').animate({
+    height: '210px',
+    width: '550px',
+    opacity: 0.8,
+  }, animationTime);
 
-  // Move it around with some function
+  // $('#p_animation').fadeIn(6000, () => $(p_animation).text('The Text Has Changed!').fadeIn(6000));
 
-  // Make it disappear
+  $('#p_animation').fadeIn(500);
+  $('#p_animation').animate({
+    fontSize: '40px',
+    color: 'white',
+  }, animationTime, () => {
+    // $(p_animation).text('Goodbye!');
+    // $('#p_animation').fadeIn(500);
+  });
 
-  // $("#animation").animate({}, )
+  $('#p_animation').text('Goodbye!');
+
+  $('#p_animation').fadeOut(500);
+  $('#p_animation').animate({
+    fontSize: '40px',
+    color: 'white',
+  }, animationTime);
+
+  $('#animation').animate({
+    height: '0px',
+    width: '0px',
+    opacity: 0,
+  }, animationTime);
+  $('#p_animation').text('Well hello there');
 };
 const handleAsyncClick = e => {
   logHistory(e, 'Async request');
   $("#p_lucky").text("Getting your lucky number...");
   $.get('/async', response => $("#p_lucky").text(response));
 };
-const handleRotationClick = e => {
-  logHistory(e, 'Rotation change');
-  // TODO
-  console.log('Rotation Click');
-  $("#image").attr("animation", "spin-clockwise infinite 20s linear");
+const handleHistoryClick = e => {
+  logHistory(e, 'View History');
+  $('#history').animate({
+    height: 'toggle',
+  });
+  // TODO: Some fade in anim for the display table
 };
+const handleSmsClick = e => {
+  logHistory(e, 'Send SMS');
+}
 
 const greeting = () => {
   const currentHour = new Date().getHours();
@@ -49,11 +79,39 @@ $(document).ready(() => {
   $("#btn_alert").click(handleAlertClick);
   $("#btn_animation").click(handleAnimationClick);
   $("#btn_async").click(handleAsyncClick);
+  $('#btn_sms').click(handleSmsClick);
 
-  $("#btn_rotate").click(handleRotationClick);
+  $("#btn_history").click(handleHistoryClick);
 
-  $("#icon_history").click(e => {
-    $("#history").text(JSON.stringify(eventHistory.map(h => { return { type: h.type, timeStamp: h.event?.timeStamp } })));
+  const source = {
+    localdata: eventHistory,
+    datatype: 'array',
+    datafields: [
+      { name: 'type', type: 'string' },
+      { name: 'event', type: 'object' },
+      { name: 'timeStamp', type: 'number' },
+      { name: 'clientX', type: 'number' },
+      { name: 'clientY', type: 'number' },
+      // { name: 'total', type: 'number' }
+    ]
+  };
+
+  const dataAdapter = new $.jqx.dataAdapter(source);
+
+  $("#jqxgrid").jqxGrid({
+    width: 850,
+    source: dataAdapter,
+    columnsresize: true,
+    columns: [
+      { text: 'Type', datafield: 'type', width: 120 },
+      { text: 'Time Stamp', datafield: 'timeStamp', width: 120 },
+      { text: 'Client X', datafield: 'clientX', width: 120 },
+      { text: 'Client Y', datafield: 'clientY', width: 120 },
+      // { text: 'Product', datafield: 'productname', width: 180 },
+      // { text: 'Quantity', datafield: 'quantity', width: 80, cellsalign: 'right' },
+      // { text: 'Unit Price', datafield: 'price', width: 90, cellsalign: 'right', cellsformat: 'c2' },
+      // { text: 'Total', datafield: 'total', cellsalign: 'right', cellsformat: 'c2' }
+    ]
   });
 });
 
